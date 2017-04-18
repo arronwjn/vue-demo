8fdafc166bd6eddff69b78af922cba7eea138826
wangjiannan <904089747@qq.com>
2017-04-18 17:16:26 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './style/App.css';
+
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <p className={style.title}>App</p>
+      </div>
+    )
+  }
+}
+
+
+export default App;
