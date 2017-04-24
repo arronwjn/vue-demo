62845153f50f71b88a5d2fb8d69793380d067bc3
wangjiannan <904089747@qq.com>
2017-04-24 21:09:40 +0800

    add onscroll

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,37 +6,14 @@ import Footer from '../.././components/Footer'
 
 
 class Home extends React.Component{
-  componentDidMount(){
-    // 1)获取元素
-    // var topNav = document.getElementById("topNav");
-    // 2)当滚动到指定位置是给#topNav添加fixed类
-    // 绑定滚动事件 (gunlun/拖动滚动条)时执行函数中的代码
-    window.onscroll = ()=>{
-    console.log('1111');
-
-    // 获取滚动条滚动过的距离
-    // var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-
-    var scrollTop = document.addEventListener("scroll", function(){
-      console.log('111')
-    });
-    console.log(scrollTop)
-    // 当滚动到<指定位置>时给#topNav添加fixed类
-    if(scrollTop >10){
-    //给元素添加类:ele.className = 'xx';
-    // topNav.className = 'fixed';
-    // this.handleScroll()
+  myFunction(){
     console.log('111')
-    }
-
   }
-
-}
   render(){
     return(
       <div className='mywarp' ref="userOrder">
         {this.props.router.isActive('/',true)?<HomeHeader/>:<FindHeader/>}
-        <div className='mid' id='topNav'>
+        <div className='mid' id='topNav' onScroll={this.myFunction.bind(this)}>
           {this.props.children}
         </div>
         <Footer/>
